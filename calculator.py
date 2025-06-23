ee81299e309867ed2de76d7492f5d49d32fed55c
komerishetty revanth <revanthgputha@gmail.com>
2025-06-23 14:10:02 +0530

    python file

--- calculator.py
+++ calculator.py
@@ -1,22 +1,22 @@
 import math
 def calculator():
-    print("Simple Calculator App")
-    print("Type 'exit' at any time to quit.")
+    print(" Calculator App")
+    print("Type 'exit' if you want to quit.")
     
     while True:
-        # Get first number
+        
         num1 = input("Enter the first number: ")
         if num1.lower() == 'exit':
             print("Exiting calculator. Goodbye!")
             break
 
-        # Get operator
+        
         operator = input("Enter an operator (+, -, *, /): ")
         if operator.lower() == 'exit':
             print("Exiting calculator. Goodbye!")
             break
 
-        # Get second number
+        
         num2 = input("Enter the second number: ")
         if num2.lower() == 'exit':
             print("Exiting calculator. Goodbye!")
